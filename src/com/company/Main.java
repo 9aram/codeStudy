2db78f350247c3d9f1195e429947172ed4e8c7a4
9aram <dlrkxks1028@naver.com>
2024-07-08 21:33:57 +0900

    basic_1 commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,8 +4,10 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-        int result =  new Solution().solution(2,3);
-        System.out.println(result);
+        //int result =  new Solution().solution(2,3);
+      //  System.out.println(result);
+
+        String str = new String();
 
     }
 }
