66ae92e3899998d570f4c4d5e278e594065b6dfc
9aram <dlrkxks1028@naver.com>
2024-07-20 18:41:06 +0900

    basic_1 commit

---
+++ src/DataStructure/ArraysAndLists/Day1_SumString.java
@@ -0,0 +1,16 @@
+package DataStructure.ArraysAndLists;
+
+import java.util.Scanner;
+
+public class Day1_SumString {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+        char[] str = sc.next().toCharArray();
+        int sum = 0;
+        for(int i=0; i<str.length; i++){
+            sum+=str[i]-'0';
+        }
+        System.out.println(sum);
+    }
+}
